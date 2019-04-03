ec61d72b4755a6f25f9729693931f5f6e17f179a
arash <arashfiroozabadii@gmail.com>
2019-04-03 23:26:08 +0430

    create AppRouter component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import Header from './components/header';
 
+import Header from './components/header';
 import './app.scss'
+
 import Footer from './components/footer';
-import Main from './components/main';
-import Search from './components/search';
+import AppRouter from './components/appRouter';
+
+
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Header />
-        <Search />
-        <Main />
+        <AppRouter />
         <Footer />
       </div>
     );
---
+++ src/components/appRouter/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Main from '../main';
+import Search from '../search';
+
+function AppRouter() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <Link to="search">جستوجو</Link>
+                    <Link to="/">خانه</Link>
+                </nav>
+                <Route exact path="/" component={Main} />
+                <Route exact path="/search" component={Search} />
+            </div>
+        </Router>
+    )
+}
+
+export default AppRouter;
