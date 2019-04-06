d4ea63fab60fa7ecb75e860ba6b16e40cd704a10
firoozabadi <arashfiroozabadii@gmail.com>
2019-04-06 15:53:45 +0430

    try create a fucking anime route

--- src/components/appRouter/index.jsx
+++ src/components/appRouter/index.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+import anime from 'animejs';
+import Anime from 'react-anime';
 import Main from '../main';
 import Search from '../search';
 import Nav from '../nav';
 
 function AppRouter() {
+   
     return (
-        <Router>
+        <Switch>
             <div>
                 <Nav />
-                <Route exact path="/" component={Main} />
-                <Route exact path="/search" component={Search} />
+                <Anime easing="easeOutElastic"
+                    loop={true}
+                    duration={1000}
+                    direction="alternate"
+                    delay={(el, index) => index * 240}
+                    scale={[.75, .9]}>
+                    <div>
+                        <Route exact path="/" component={Main} />
+                    </div>
+                </Anime>
+                <div>
+                    <Route exact path="/search" component={Search} />
+                </div>
+                
             </div>
-        </Router>
+        </Switch>
     )
 }
 
--- src/components/navLink/index.jsx
+++ src/components/navLink/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import anime from 'animejs';
+
 import './style.scss';
 
 function handlePush(e,props) {
